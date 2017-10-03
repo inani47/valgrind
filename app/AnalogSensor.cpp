986682db9a350c6fd4ad257ece7e5726e8e48e12
Pranav Inani <pranavinani94@gmail.com>
2017-10-03 13:17:03 -0700

    formatting

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -14,7 +14,7 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::shared_ptr<std::vector<int> >readings  (new std::vector<int>(mSamples, 10));
+    std::shared_ptr<std::vector<int> > readings  (new std::vector<int>(mSamples, 10));
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
 }
